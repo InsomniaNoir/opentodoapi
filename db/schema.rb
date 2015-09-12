e519199b11a862c6ea0240634b37f4ba08c71770
Jeremiah Shafer <jrshafer@mac.com>
2015-09-12 16:47:05 -0500

    Correct Migration & Serializer

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150911201036) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
