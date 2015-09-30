62aa9d5148ef0db22004edd078a315b3353c46e4
Jeremiah Shafer <jrshafer@mac.com>
2015-09-29 21:34:21 -0500

    Add permission validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,11 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :items
+  validate :permissions_valid, on: :update
+
+  def permissions_valid
+    unless permissions == "Private"
+        errors.add(:permissions, "Wrong permissions")
+    end
+  end
 end
